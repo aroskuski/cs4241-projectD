5b9b9424fcfb504a2cdd2c108745a58730e6c2b6
Andrew Roskuski <aroskuski@gmail.com>
2014-11-22 15:52:54 -0500

    initial query implementation

--- js/database.js
+++ js/database.js
@@ -5,7 +5,56 @@
 var inspect = require('util').inspect;
 var Client = require('mariasql');
 
-exports.list = function(req, res){
+function selectQuery(query){
+    var result = [];
+    var c = new Client();
 
+    c.connect({
+        host: 'rous.wpi.edu',
+        user: 'ajroskuski',
+        password: 'ajroskuski_pw',
+        db: 'ajroskuski_db'
+
+    });
+
+    c.on('connect',function(){
+        console.log('Connected to db');
+    })
+    .on('error', function (err){
+        console.log('Client Error ' + err);
+    })
+    .on('close', function(){
+        console.log('Client Closed');
+    });
+
+    c.query(query)
+        .on('result', function (res){
+            res.on('row',function(row){
+                result.push(row);
+            })
+                .on('error', function(err){
+                    console.log("result error " + inspect(err));
+                })
+                .on('end', function(){
+                    console.log('Result finished successfully')
+                });
+        })
+        .on('end', function(){
+            console.log("Done with all results");
+        });
+
+    c.end();
+
+    return result;
+}
+
+
+
+exports.requestJSON = function(req, res){
+
+
+};
+
+exports.requestTable = function(req, res){
 
 };
