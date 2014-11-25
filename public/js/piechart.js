ea98c694b529c3de897574750b822b79f566f380
Alexander W. Witt <awittscience@gmail.com>
2014-11-25 13:01:24 -0500

    Associated appropriate build function with appropriate chart generating function.

--- public/js/piechart.js
+++ public/js/piechart.js
@@ -88,54 +88,6 @@ function buildchart1(dataArray) {
             },
             // Set the title of the chart
             title: {
-                text: 'Pokemon - Nature Chart'
-            },
-            // Make it so that a tooltip appears showing the name of the series
-            // and the percentage composition of the currently hovered-over slice
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            // Add to the options list for the chart
-            plotOptions: {
-                // Allow for labeling and point selection for the pie chart
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                        }
-                    }
-                }
-            },
-            // Add the data series that will help to produce the Highchart
-            series: [{
-                type: 'pie',
-                name: 'Pokemon Nature',
-                data: dataArray
-            }]
-        });
-    });
-}
-
-
-// This Javascript function builds the second pie chart
-function buildchart2(dataArray) {
-
-    // JQuery function for populating Highchart with JSON data
-    $(function (dataArray) {
-        // Indicate that the graph1 DOM object will be updated with a Highchart
-        $('#graph1').highcharts({
-            // Prepare some stylistic properties for the chart
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: 1,
-                plotShadow: false
-            },
-            // Set the title of the chart
-            title: {
                 text: 'Pokemon - Most Commonly Held Items'
             },
             // Make it so that a tooltip appears showing the name of the series
@@ -161,7 +113,55 @@ function buildchart2(dataArray) {
             // Add the data series that will help to produce the Highchart
             series: [{
                 type: 'pie',
-                name: 'Most Commonly Held Items',
+                name: 'Pokemon - Most Commonly Held Items',
+                data: dataArray
+            }]
+        });
+    });
+}
+
+
+// This Javascript function builds the second pie chart
+function buildchart2(dataArray) {
+
+    // JQuery function for populating Highchart with JSON data
+    $(function (dataArray) {
+        // Indicate that the graph1 DOM object will be updated with a Highchart
+        $('#graph1').highcharts({
+            // Prepare some stylistic properties for the chart
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: 1,
+                plotShadow: false
+            },
+            // Set the title of the chart
+            title: {
+                text: 'Pokemon - Nature'
+            },
+            // Make it so that a tooltip appears showing the name of the series
+            // and the percentage composition of the currently hovered-over slice
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            // Add to the options list for the chart
+            plotOptions: {
+                // Allow for labeling and point selection for the pie chart
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            // Add the data series that will help to produce the Highchart
+            series: [{
+                type: 'pie',
+                name: 'Nature',
                 data: dataArray
             }]
         });
