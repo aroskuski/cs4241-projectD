6f1f4a56b7264aef0d338daf6b9e30865f9b5227
Andrew Roskuski <aroskuski@gmail.com>
2014-12-02 00:20:42 -0500

    Comments + Net ball png

--- js/database.js
+++ js/database.js
@@ -5,11 +5,13 @@
 var inspect = require('util').inspect;
 var Client = require('mariasql');
 
+//Runs the given query, then executes resultfunc
 function runQuery(query, res, resultfunc){
     var out = [];
     var c = new Client();
     //var query;
 
+    //login
     c.connect({
         host: '127.0.0.1',
         user: 'ajroskuski',
@@ -18,11 +20,9 @@ function runQuery(query, res, resultfunc){
 
     });
 
+    //event handlers
     c.on('connect',function(){
         console.log('Connected to db');
-
-
-
     })
     .on('error', function (err){
         console.log('Client Error ' + err);
@@ -31,6 +31,7 @@ function runQuery(query, res, resultfunc){
         console.log('Client Closed');
     });
 
+    //run query
     c.query(query)
         .on('result', function (result){
             result.on('row',function(row){
@@ -38,7 +39,8 @@ function runQuery(query, res, resultfunc){
             })
                 .on('error', function(err){
                     console.log("result error " + inspect(err));
-                    res.status(400);
+                    res.status(400);// Something has gone wrong
+                                    // Since all the SQL queries are hardcoded, it is probably not the server's fault
                 })
                 .on('end', function(){
                     console.log('Result finished successfully')
@@ -46,6 +48,7 @@ function runQuery(query, res, resultfunc){
         })
         .on('end', function(){
             console.log("Done with all results");
+            //do callback
             resultfunc(res, out);
         });
 
@@ -58,7 +61,7 @@ function runQuery(query, res, resultfunc){
 }
 
 
-
+//Wrapper for runquery to return JSON as the response
 function requestJSON(query, res){
     console.log(query);
     runQuery(query, res,  function (res, result){
@@ -80,36 +83,41 @@ function requestTable(query , res){
 }
 */
 
-
+// Receive form data and insert into db
 exports.postData = function (req, res) {
     //req.body
     //var query = "INSERT INTO div1 (pdexID, item, nature) VALUES (";
     var PokedexNo;
     var natureID;
+    // Assume everything is going right until something goes wrong
     res.status(204);
     console.log("name " + req.body.name);
     console.log("item " + req.body.item);
     console.log("nature " + req.body.nature);
+    //  Weird stuff happens when any of these is undefined, so this is just to short circuit that
     if(req.body.name == undefined || req.body.item == undefined || req.body.nature == undefined){
         res.status(400).send();
         return;
     }
     console.log("SELECT * FROM pkmn WHERE Name=\'" + req.body.name + '\';');
+    //Get the pokedex number that corresponds to req.body.name
     runQuery("SELECT * FROM pkmn WHERE Name=\'" + req.body.name + '\';', res,  function (res, result){
         console.log(JSON.stringify(result));
-        if (result[0] != undefined){
+        if (result[0] != undefined){//Check if a result exists
             PokedexNo = result[0].PokedexNo;
         } else {
-            PokedexNo = 0;
+            PokedexNo = 0;// This is invalid, there is no number zero
         }
         console.log("SELECT * FROM nature WHERE nature=\'" + req.body.nature + '\';');
+        // Get the id of the nature in the post request
         runQuery("SELECT * FROM nature WHERE nature=\'" + req.body.nature + '\';', res, function(res, result) {
             console.log(JSON.stringify(result));
-            if (result[0] != undefined){
+            if (result[0] != undefined){// Check if result exists
                 natureID = result[0].id;
             } else {
-                natureID = 100;
+                natureID = 100;// there are only like 20-something natures, so this will always be invalid
             }
+            //build query string
             var query = "INSERT INTO div1 (pdexID, item, nature) VALUES (";
             query += PokedexNo;
             query += ',\'';
@@ -118,9 +126,11 @@ exports.postData = function (req, res) {
             query += natureID;
             query += ');';
             console.log(query);
+            //run the insert query
             runQuery(query, res, function(res, result){
                 console.log(JSON.stringify(result));
-
+                // result will always be []
+                // status code is all we are sending back, and that has already been set appropriately
                 res.send();
             });
             //res.set('Content-Type', 'application/json');
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,18 @@ var dbinterface = require('../js/database.js');
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+*/
 
-
+//Main dashboard html file
 router.get('/dashboard.html', function(req, res){
   //dbinterface.requestJSON(req,res);
   res.render('dashboard', { title: 'Dashboard' });
 });
 
+//Paragraphs to load into dashboard
 router.get('/paragraph/:id', function(req, res){
   fs.readFile("text/text" + req.params.id + ".txt", "utf8", function(err,data) {// read file
     //console.log(err);
@@ -27,8 +29,10 @@ router.get('/paragraph/:id', function(req, res){
   });
 });
 
+//Post data into database
 router.post('/postdata', dbinterface.postData);
 
+// All .json routes are really just wrappers for SQL queries
 router.get('/helditem.json', dbinterface.heldItem);
 
 router.get('/nature.json', dbinterface.nature);
@@ -37,8 +41,6 @@ router.get('/popular.json', dbinterface.popular);// requires query string ?id=<n
 
 router.get('/moveclass.json', dbinterface.moveClass);//optional query sting ?type=<type name(ex: fire water, etc)>
 
-//router.get('/table', dbinterface.requestTable);
 
-//router.get('/json', dbinterface.requestJSON);
 
 module.exports = router;
